1b634898a173ccff4caf0e7641ac7cb6a63b36f2
jangamreddyprathyusha <jangamreddyprathu@gmail.com>
2026-06-30 18:37:53 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,37 @@
+# a=int(0)
+# print(a)
+# print(type(a))
+
+# b=float(0)
+# print(b)
+
+# c=complex(0)
+# print(c)
+
+# #string is odrder,immutable and indexing
+# s=0
+# print(type(s))
+
+# l=[0]
+# print(l)
+
+#power of 2
+# n=int(input("Enter a number: "))
+# while n > 1:
+#         if n % 2 == 0:
+#             print(True)
+#         n //= 2
+           #(or) 
+# n=int(input("Enter a number: "))
+# print(n>0 and (n&(n-1)==0))  
+
+
+
+#power of 3
+n=int(input("Enter a number: "))
+while n > 1:
+        if n % 3 == 0:
+            print(True)
+        n //= 3
+print(n==1)
+        
